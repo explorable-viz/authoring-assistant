2a1afa82497282df97bf27c5ddcd71f52995335d
Alfonso <alfonso.piscitelli91@gmail.com>
2025-05-15 09:12:34 +0100

    [update] space and parenthesis management

--- src/main/java/explorableviz/authoringassistant/paragraph/Paragraph.java
+++ src/main/java/explorableviz/authoringassistant/paragraph/Paragraph.java
@@ -14,7 +14,7 @@ public class Paragraph extends ArrayList<TextFragment> {
     }
 
     public String toFluidSyntax() {
-        return STR."Paragraph([\n\t\{stream().map(e -> {
+        return STR."Paragraph[\n\t\{stream().map(e -> {
             if (e instanceof Literal l && l.getSelectedRegion() != null)
             {
                 return STR."\{e.getValue().substring(0, l.getSelectedRegion().start())} [REPLACE \{Settings.isAddExpectedValueEnabled() ? STR."value=\"\{e.getValue()}\"" : ""}]\{e.getValue().substring(l.getSelectedRegion().end())}";
@@ -22,12 +22,12 @@ public class Paragraph extends ArrayList<TextFragment> {
             else if (e instanceof Literal) return STR."Text \"\{e.getValue()}\"";
             else if (e instanceof Expression) {
                 if(e.getValue().matches("-?\\d+(\\.\\d+)?"))
-                    return (STR."Text(numToStr(\{((Expression) e).getExpr()}))");
+                    return (STR."Text (numToStr (\{((Expression) e).getExpr()}))");
                 else
-                    return (STR."Text(\{((Expression) e).getExpr()})");
+                    return (STR."Text (\{((Expression) e).getExpr()})");
             }
             throw new RuntimeException("Error, it is possible to have only String, Expression element");
-        }).collect(Collectors.joining(",\n\t"))}\n])";
+        }).collect(Collectors.joining(",\n\t"))}\n]";
     }
     public List<Pair<Expression, Paragraph>> asIndividualEdits(Paragraph template) {
         final int numComputedExpr;

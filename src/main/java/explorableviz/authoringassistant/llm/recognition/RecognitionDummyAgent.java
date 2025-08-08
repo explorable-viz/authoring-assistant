c0496360e766c1ea77279720763095457a81e5cc
Alfonso <alfonso.piscitelli91@gmail.com>
2025-08-08 13:11:26 +0200

    [new] llm agents for recognition agent

---
+++ src/main/java/explorableviz/authoringassistant/llm/recognition/RecognitionDummyAgent.java
@@ -0,0 +1,23 @@
+package explorableviz.authoringassistant.llm.recognition;
+
+import explorableviz.authoringassistant.paragraph.Expression;
+import it.unisa.cluelab.lllm.llm.LLMEvaluatorAgent;
+import it.unisa.cluelab.lllm.llm.prompt.PromptList;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class RecognitionDummyAgent extends LLMEvaluatorAgent<String> {
+    public static Logger logger = Logger.getLogger(RecognitionDummyAgent.class.getName());
+    public RecognitionDummyAgent(JSONObject settings) {
+        super(settings);
+    }
+
+    @Override
+    public String evaluate(PromptList list, String s) throws IOException {
+        logger.info("Execution of the DummyAgent");
+        list.addSystemPrompt("dummy");
+        return "Test paragraph with one replace tag with value [REPLACE value=\"10\"].";
+    }
+}

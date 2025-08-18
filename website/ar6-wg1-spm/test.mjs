154fd4aa43473227fa302d14f491108caea43fcf
Jessy Sodimu <jessysodimu@gmail.com>
2025-08-18 17:02:12 +0100

    update website tests

--- website/ar6-wg1-spm/test.mjs
+++ website/ar6-wg1-spm/test.mjs
@@ -1,7 +1,7 @@
 import { runTests, testURL, waitForFigure, waitFor } from "./shared/webtest-lib.js";
 
 export const main = async () => {
-    await runTests(testURL("figure-spm4b")([ (page) => waitForFigure(page)("fig-leftBarChart") ]))();
-    await runTests(testURL("table-spm1")([ (page) => waitFor("text#fig-explanation119")(page) ]))();
+    await runTests(testURL("figure-spm4b")([ (page) =>  waitForFigure(page)("")]))();
+    await runTests(testURL("table-spm1")([ (page) => waitFor("#fig-output")(page) ]))();
     console.log("Success!");
 };

1c9bf35ce032e91730d031241b42637d28939e25
Jessy Sodimu <jessysodimu@gmail.com>
2025-08-19 09:42:27 +0100

    deprecate waitForFigure

--- website/ar6-wg1-spm/test.mjs
+++ website/ar6-wg1-spm/test.mjs
@@ -1,7 +1,7 @@
-import { runTests, testURL, waitForFigure, waitFor } from "./shared/webtest-lib.js";
+import { runTests, testURL, waitFor } from "./shared/webtest-lib.js";
 
 export const main = async () => {
-    await runTests(testURL("figure-spm4b")([ (page) =>  waitForFigure(page)("")]))();
+    await runTests(testURL("figure-spm4b")([ (page) =>  waitFor("svg")(page)]))();
     await runTests(testURL("table-spm1")([ (page) => waitFor("#fig-output")(page) ]))();
     console.log("Success!");
 };

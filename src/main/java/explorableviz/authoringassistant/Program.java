6d8362c6d50a9e3dffc7ccbfbd2c4df250b25a3b
Roly Perera <roly.perera@dynamicaspects.org>
2025-08-22 14:33:46 +0100

    Fix extensions on .fld files.

--- src/main/java/explorableviz/authoringassistant/Program.java
+++ src/main/java/explorableviz/authoringassistant/Program.java
@@ -372,7 +372,7 @@ public class Program {
             e.printStackTrace();
         }
         /* copy datasets  & lib */
-        writeFluidFiles(STR."\{path}fluid/", Path.of(this.testCaseFileName).getFileName().toString(), paragraph.toFluidSyntax(false), datasets, _loadedDatasets, imports, _loadedImports, code);
+        writeFluidFiles(STR."\{path}fluid/", STR."\{Path.of(this.testCaseFileName).getFileName()}.fld", paragraph.toFluidSyntax(false), datasets, _loadedDatasets, imports, _loadedImports, code);
     }
 
     public static void cleanWebsiteFolders(String path) {

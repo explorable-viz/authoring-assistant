fe97ec7ea5c51420348ac8e377e93427d07d8c08
rolyp <rolyp@users.noreply.github.com>
2025-09-08 09:04:44 +0000

    deploy: 1118126175194d26071b0ecb9b298dc890104a6f

--- test.mjs
+++ test.mjs
@@ -1,11 +1,11 @@
-import { runTests, testURL, waitFor } from "./shared/webtest-lib.js";
+import { runTests, testURL, waitFor } from "./shared/webtest-lib.js"
 
 export const main = async () => {
-    await runTests(testURL("scigen-multiple-replace-1805.02474v1-10")([ page => waitFor("div#fig-output")(page) ]))();
-    await runTests(testURL("scigen-multiple-replace-1805.02474v1-14")([ (page) => waitFor("div#fig-output")(page) ]))();
-    await runTests(testURL("scigen-multiple-replace-1904.12550v1-5")([ (page) => waitFor("div#fig-output")(page) ]))();
-    await runTests(testURL("scigen-multiple-replace-1904.12550v1-6")([ (page) => waitFor("div#fig-output")(page) ]))();
-    await runTests(testURL("scigen-multiple-replace-1906.02780v1-17")([ (page) => waitFor("div#fig-output")(page) ]))();
-    await runTests(testURL("scigen-multiple-replace-ipcc")([ (page) => waitFor("div#fig-output")(page) ]))();
-    console.log("Success!");
-};
+    await runTests(testURL("scigen-multiple-replace-1805.02474v1-10")([ page => waitFor("div#fig-output")(page) ]))()
+    await runTests(testURL("scigen-multiple-replace-1805.02474v1-14")([ (page) => waitFor("div#fig-output")(page) ]))()
+    await runTests(testURL("scigen-multiple-replace-1904.12550v1-5")([ (page) => waitFor("div#fig-output")(page) ]))()
+    await runTests(testURL("scigen-multiple-replace-1904.12550v1-6")([ (page) => waitFor("div#fig-output")(page) ]))()
+    await runTests(testURL("scigen-multiple-replace-1906.02780v1-17")([ (page) => waitFor("div#fig-output")(page) ]))()
+    await runTests(testURL("scigen-multiple-replace-ipcc")([ (page) => waitFor("div#fig-output")(page) ]))()
+    console.log("Success!")
+}

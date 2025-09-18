2849980ce3551f9a59d5164f854f79c4ba311104
Alfonso <alfonso.piscitelli91@gmail.com>
2025-09-18 13:03:02 +0200

    [new] LLM agents for suggestions

---
+++ src/main/java/explorableviz/authoringassistant/llm/recognition/CodeLLamaAgent.java
@@ -0,0 +1,18 @@
+package explorableviz.authoringassistant.llm.recognition;
+
+import explorableviz.authoringassistant.paragraph.Expression;
+import it.unisa.cluelab.lllm.llm.agents.generic.OLLAMAEvaluatorAgent;
+import org.json.JSONObject;
+
+public class CodeLLamaAgent extends OLLAMAEvaluatorAgent<String> {
+
+    public CodeLLamaAgent(JSONObject setttings) {
+        super(setttings);
+        setModel("codellama:34b");
+    }
+
+    @Override
+    public String parse(String s) {
+        return s;
+    }
+}

789eab6f732bfdaa9e17ccd193886d78d978ee4f
Jessy Sodimu <jessysodimu@gmail.com>
2025-11-26 15:36:59 +0000

    tidy generate-scigen-manual

--- script/generate-scigen-manual.sh
+++ script/generate-scigen-manual.sh
@@ -2,4 +2,4 @@
 set -xe
 
 ./script/downsample.sh ./testCases/scigen-SuggestionAgent-raw/ ./testCases/scigen-manual-2/
-#.//script/downsample.sh ./testCases/scigen-manual-2/ ./testCases/scigen-manual-final/
+

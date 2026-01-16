3b6c6b214aad1016bcc6e6fe415cc09eddf76a1e
Roly Perera <roly.perera@dynamicaspects.org>
2026-01-16 11:44:27 +0000

    --quiet on pip.

--- script/test.sh
+++ script/test.sh
@@ -3,5 +3,5 @@ set -xe
 
 ./script/authoring-assistant.sh config=test
 
-python3 -m pip install --user -r script/requirements.txt --disable-pip-version-check
+python3 -m pip install --user -r script/requirements.txt --quiet --disable-pip-version-check
 python3 script/generate-charts.py test

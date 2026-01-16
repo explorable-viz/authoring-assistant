80c6a046ec284b620acc8fe10253525e8168d8cd
Roly Perera <roly.perera@dynamicaspects.org>
2026-01-16 11:58:35 +0000

    Try removing --user.

--- script/test-mock.sh
+++ script/test-mock.sh
@@ -3,5 +3,5 @@ set -xe
 
 ./script/authoring-assistant.sh config=test-mock
 
-python3 -m pip install --user -r script/requirements.txt --quiet --disable-pip-version-check
+python3 -m pip install -r script/requirements.txt --quiet --disable-pip-version-check
 python3 script/generate-charts.py test-mock # degenerate but useful to validate script

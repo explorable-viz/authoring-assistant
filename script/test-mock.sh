622a31bbc7fc8d6c1ca0de4a2f0f333011d23da2
Roly Perera <roly.perera@dynamicaspects.org>
2026-01-16 12:14:54 +0000

    venv.

--- script/test-mock.sh
+++ script/test-mock.sh
@@ -3,5 +3,7 @@ set -xe
 
 ./script/authoring-assistant.sh config=test-mock
 
-python3 -m pip install -r script/requirements.txt --quiet --disable-pip-version-check
-python3 script/generate-charts.py test-mock # degenerate but useful to validate script
+python3 -m venv .venv
+.venv/bin/python -m pip install -r script/requirements.txt \
+  --quiet --disable-pip-version-check
+.venv/bin/python script/generate-charts.py test-mock # degenerate but useful to validate script

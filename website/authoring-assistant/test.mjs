adb1b84172e542827a853550f7c3b1d0650f049c
Roly Perera <roly.perera@dynamicaspects.org>
2026-01-20 17:00:22 +0000

    Fix up URLs.

--- website/authoring-assistant/test.mjs
+++ website/authoring-assistant/test.mjs
@@ -1,9 +1,9 @@
 import { runTests, testURL, waitFor } from "./shared/webtest-lib.js"
 
 export const main = async () => {
-    await runTests(testURL("scigen-manual/1002.0773")([ page => waitFor("div#fig-output")(page) ]))()
-    await runTests(testURL("scigen-manual/1003.0206")([ page => waitFor("div#fig-output")(page) ]))()
-    await runTests(testURL("scigen-manual/1110.3094")([ page => waitFor("div#fig-output")(page) ]))()
+    await runTests(testURL("scigen-manual/1002.0773-8586")([ page => waitFor("div#fig-output")(page) ]))()
+    await runTests(testURL("scigen-manual/1003.0206-7624")([ page => waitFor("div#fig-output")(page) ]))()
+    await runTests(testURL("scigen-manual/1110.3094-5931")([ page => waitFor("div#fig-output")(page) ]))()
     await runTests(testURL("scigen-manual/1805.02474v1-10")([ page => waitFor("div#fig-output")(page) ]))()
     await runTests(testURL("scigen-manual/1805.02474v1-14")([ (page) => waitFor("div#fig-output")(page) ]))()
     await runTests(testURL("scigen-manual/1807.07279v3-23")([ (page) => waitFor("div#fig-output")(page) ]))()
